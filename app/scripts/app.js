7c057d6b28a415605e8e6fc86be6f71916495967
konartur <suntikflood@gmail.com>
2021-10-26 16:10:42 +0300

    first commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,9 +2,9 @@ angular.module('myApp', ['ngRoute', 'app.homePages'])
 
   .constant('TPL_PATH', '/templates')
 
-  .config(function($routeProvider, TPL_PATH) {
-    $routeProvider.when('/',{
-      controller : 'HomeCtrl',
-      templateUrl : TPL_PATH + '/home.html'
+  .config(function ($routeProvider, TPL_PATH) {
+    $routeProvider.when('/', {
+      controller: 'ToDoListCtrl',
+      templateUrl: TPL_PATH + '/home.html'
     });
   });
--- app/scripts/homePages.js
+++ app/scripts/homePages.js
@@ -1,11 +1,42 @@
+var listToDo = {
+  user: "Артур",
+  toDo: [
+    // { description: "Что-то сделать", state: true },
+    // { description: "Что-то сделать2", state: false },
+    // { description: "Что-то сделать3", state: false },
+    // { description: "Что-то сделать4", state: true },
+  ],
+};
+
 angular.module('app.homePages', [])
+  .controller('ToDoListCtrl', function ($scope) {
+    $scope.data = listToDo;
+    $scope.validation = function (value) {
+      return value ? true : false;
+    }
 
-  .factory('welcomeMessage', function() {
-    return function() {
-      return 'Welcome Home...';
-    };
-  })
+    $scope.addNewToDo = function () {
+      $scope.data.toDo.push({
+        description: $scope.toDoName,
+        time: $scope.timeForToDo,
+        state: false,
+      });
 
-  .controller('HomeCtrl', function($scope, welcomeMessage) {
-    $scope.welcome_message = welcomeMessage();
+      $scope.toDoName = "";
+      $scope.timeForToDo = "";
+    }
+
+    $scope.setStyle = function (state) {
+      return state ? "color: green" : "color: red";
+    }
+
+    $scope.showText = function (state) {
+      return state ? "Выполнено" : "В процессе";
+    }
+
+    $scope.delete = function () {
+      $scope.data.toDo.splice(this.toDo, 1);
+    }
   });
+
+// https://metanit.com/web/angular/2.2.php
